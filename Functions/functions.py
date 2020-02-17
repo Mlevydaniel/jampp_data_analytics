5c9ad3c2a49585702e07ed8c56b59b2e8fbc5a0f
martinlevydaniel <mlevydaniel@gmail.com>
2020-02-17 15:20:47 -0300

    Exercise 1 solved

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,9 +1,36 @@
+import random
+
 def weighted_random(values, weights):
 	total_weight = sum(weights)
 	acum_weights = [w / total_weight for w in weights[:]]
-	for i in range(len(weights)):
-		acum_weights[i] += acum_weights[i]
+
+	# accumulates weights
+	for i in range(1, len(weights)):
+		acum_weights[i] += acum_weights[i]  # this is not ok!!
 	rand = random.random()
+
+	# returns value if weight is higher than random value
 	for value, weight in zip(values, acum_weights):
 		if weight > rand:
 			return value
+
+def new_weighted_random(values, weights):
+	total_weight = sum(weights)
+	acum_weights = [w / total_weight for w in weights[:]]
+
+	# accumulates weights
+	for i in range(1, len(weights)):
+		acum_weights[i] += acum_weights[i-1] # Now it works properly
+	rand = random.random()
+
+	# returns value if weight is higher than random value
+	for value, weight in zip(values, acum_weights):
+		if weight > rand:
+			return value
+
+print('library loaded')
+
+if __name__=='__main__':
+	values = [1, 2, 3]
+	weights = [0.5, 0.3, 0.2]
+	new_weighted_random(values, weights)
